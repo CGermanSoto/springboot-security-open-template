70da36fb58ec596879d2f3ff5a4eeafaf7b73110
spacecodee <spacecodee@gmail.com>
2024-10-24 23:20:37 -0500

    Starting the project condiguration

---
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/SpringbootSecurityOpenTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.spacecodee.springbootsecurityopentemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityOpenTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurityOpenTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spacecodee/springbootsecurityopentemplate/SpringbootSecurityOpenTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacecodee.springbootsecurityopentemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityOpenTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
