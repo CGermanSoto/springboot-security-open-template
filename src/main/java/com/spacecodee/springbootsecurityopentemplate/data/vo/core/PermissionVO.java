aeab66dea6a2d38094c3221486a37ff18ef35723
spacecodee <spacecodee@gmail.com>
2024-12-15 05:59:44 +0000

    Update validation messages in PermissionVO for improved clarity and consistency

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/data/vo/core/PermissionVO.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/data/vo/core/PermissionVO.java
@@ -1,13 +1,20 @@
 package com.spacecodee.springbootsecurityopentemplate.data.vo.core;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class PermissionVO {
-    @NotNull(message = "{validation.permission.role.required}")
+    @NotNull(message = "{validation.permission.role.required," +
+            "${validatedValue}")
+    @Min(value = 1, message = "{validation.permission.role.min," +
+            "${validatedValue}")
     private Integer roleId;
 
-    @NotNull(message = "{validation.permission.operation.required}")
+    @NotNull(message = "{validation.permission.operation.required," +
+            "${validatedValue}")
+    @Min(value = 1, message = "{validation.permission.operation.min," +
+            "${validatedValue}")
     private Integer operationId;
 }
