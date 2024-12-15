1dfb57601d510f0ec0361f469248d39a107ab1bc
spacecodee <spacecodee@gmail.com>
2024-12-15 04:05:03 +0000

    Refactor AbstractController to utilize MessageParameterHandler for improved message handling and logging in response creation

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/controller/base/AbstractController.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/controller/base/AbstractController.java
@@ -2,25 +2,53 @@ package com.spacecodee.springbootsecurityopentemplate.controller.base;
 
 import com.spacecodee.springbootsecurityopentemplate.data.common.response.ApiResponseDataPojo;
 import com.spacecodee.springbootsecurityopentemplate.data.common.response.ApiResponsePojo;
+import com.spacecodee.springbootsecurityopentemplate.language.MessageParameterHandler;
 import com.spacecodee.springbootsecurityopentemplate.language.MessageUtilComponent;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 
+import java.util.Arrays;
+
 @RequiredArgsConstructor
+@Slf4j
 public abstract class AbstractController {
     protected final MessageUtilComponent messageUtilComponent;
+    protected final MessageParameterHandler messageParameterHandler;
 
     protected ApiResponsePojo createResponse(String messageKey, String locale, HttpStatus status) {
+        log.debug("Creating response: messageKey='{}', locale='{}', status='{}'",
+                messageKey, locale, status);
         return ApiResponsePojo.of(
-                this.messageUtilComponent.getMessage(messageKey, locale),
+                this.messageParameterHandler.createMessage(messageKey, locale),
                 status);
     }
 
-    protected <T> ApiResponseDataPojo<T> createDataResponse(@Valid T data, String messageKey, String locale,
-                                                            HttpStatus status) {
+    protected ApiResponsePojo createResponse(String messageKey, String locale, HttpStatus status, Object... params) {
+        log.debug("Creating parameterized response: messageKey='{}', locale='{}', status='{}', params='{}'",
+                messageKey, locale, status, Arrays.toString(params));
+        return ApiResponsePojo.of(
+                this.messageParameterHandler.createMessage(messageKey, locale, params),
+                status);
+    }
+
+    protected <T> ApiResponseDataPojo<T> createDataResponse(@Valid T data, String messageKey,
+            String locale, HttpStatus status) {
+        log.debug("Creating data response: messageKey='{}', locale='{}', status='{}', data='{}'",
+                messageKey, locale, status, data);
         return ApiResponseDataPojo.of(
-                this.messageUtilComponent.getMessage(messageKey, locale),
+                this.messageParameterHandler.createMessage(messageKey, locale),
+                status,
+                data);
+    }
+
+    protected <T> ApiResponseDataPojo<T> createDataResponse(@Valid T data, String messageKey,
+            String locale, HttpStatus status, Object... params) {
+        log.debug("Creating parameterized data response: messageKey='{}', locale='{}', status='{}', params='{}'",
+                messageKey, locale, status, Arrays.toString(params));
+        return ApiResponseDataPojo.of(
+                this.messageParameterHandler.createMessage(messageKey, locale, params),
                 status,
                 data);
     }
