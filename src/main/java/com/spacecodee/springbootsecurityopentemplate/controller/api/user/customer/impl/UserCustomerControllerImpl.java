c629f1237db5db96f75b9a185c5c48fe7f1896e4
spacecodee <spacecodee@gmail.com>
2024-12-15 04:06:11 +0000

    Refactor UserCustomerControllerImpl to integrate MessageParameterHandler and enhance response messages with usernames for improved clarity

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/controller/api/user/customer/impl/UserCustomerControllerImpl.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/controller/api/user/customer/impl/UserCustomerControllerImpl.java
@@ -7,6 +7,7 @@ import com.spacecodee.springbootsecurityopentemplate.data.common.response.ApiRes
 import com.spacecodee.springbootsecurityopentemplate.data.dto.user.CustomerDTO;
 import com.spacecodee.springbootsecurityopentemplate.data.vo.user.customer.CustomerAVO;
 import com.spacecodee.springbootsecurityopentemplate.data.vo.user.customer.CustomerUVO;
+import com.spacecodee.springbootsecurityopentemplate.language.MessageParameterHandler;
 import com.spacecodee.springbootsecurityopentemplate.language.MessageUtilComponent;
 import com.spacecodee.springbootsecurityopentemplate.service.core.user.customer.IUserCustomerService;
 import org.springframework.http.HttpStatus;
@@ -23,8 +24,9 @@ public class UserCustomerControllerImpl extends AbstractController implements IU
     private final IUserCustomerService userClientService;
 
     public UserCustomerControllerImpl(MessageUtilComponent messageUtilComponent,
+                                      MessageParameterHandler messageParameterHandler,
                                       IUserCustomerService userClientService) {
-        super(messageUtilComponent);
+        super(messageUtilComponent, messageParameterHandler);
         this.userClientService = userClientService;
     }
 
@@ -33,36 +35,36 @@ public class UserCustomerControllerImpl extends AbstractController implements IU
         this.userClientService.add(request, locale);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(createResponse("customer.added.success", locale, HttpStatus.CREATED));
+                .body(createResponse("customer.added.success", locale,
+                        HttpStatus.CREATED, request.getUsername()));
     }
 
     @Override
     public ResponseEntity<ApiResponsePojo> update(String locale, int id, CustomerUVO request) {
         this.userClientService.update(id, request, locale);
-        return ResponseEntity.ok(createResponse("customer.updated.success", locale, HttpStatus.OK));
+        return ResponseEntity.ok(createResponse("customer.updated.success",
+                locale, HttpStatus.OK, request.getUsername()));
     }
 
     @Override
     public ResponseEntity<ApiResponsePojo> delete(String locale, int id) {
+        var customer = this.userClientService.findById(id, locale);
         this.userClientService.delete(id, locale);
-        return ResponseEntity.ok(createResponse("customer.deleted.success", locale, HttpStatus.OK));
+        return ResponseEntity.ok(createResponse("customer.deleted.success",
+                locale, HttpStatus.OK, customer.username()));
     }
 
     @Override
     public ResponseEntity<ApiResponseDataPojo<CustomerDTO>> findById(String locale, int id) {
-        return ResponseEntity.ok(createDataResponse(
-                this.userClientService.findById(id, locale),
-                "customer.found.success",
-                locale,
-                HttpStatus.OK));
+        var customer = this.userClientService.findById(id, locale);
+        return ResponseEntity.ok(createDataResponse(customer,
+                "customer.found.success", locale, HttpStatus.OK, customer.username()));
     }
 
     @Override
     public ResponseEntity<ApiResponseDataPojo<List<CustomerDTO>>> findAll(String locale) {
-        return ResponseEntity.ok(createDataResponse(
-                this.userClientService.findAll(locale),
-                "customer.list.success",
-                locale,
-                HttpStatus.OK));
+        var customers = this.userClientService.findAll(locale);
+        return ResponseEntity.ok(createDataResponse(customers,
+                "customer.list.success", locale, HttpStatus.OK, customers.size()));
     }
 }
