b24defac5fe020c3fc7ba4440823214583c91965
spacecodee <spacecodee@gmail.com>
2024-12-15 05:59:13 +0000

    Update validation messages in LoginUserVO for improved clarity and consistency

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/data/vo/auth/LoginUserVO.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/data/vo/auth/LoginUserVO.java
@@ -5,9 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record LoginUserVO(
-        @NotBlank(message = "{validation.login.username.required}") @Size(min = ValidationConstants.MIN_USERNAME_LENGTH, max = ValidationConstants.MAX_USERNAME_LENGTH, message = "{"
-                + ValidationConstants.Messages.USERNAME_SIZE + "}") String username,
+        @NotBlank(message = "{validation.login.username.required}") @Size(min = ValidationConstants.MIN_USERNAME_LENGTH, max = ValidationConstants.MAX_USERNAME_LENGTH, message = "{validation.user.username.size}") String username,
 
-        @NotBlank(message = "{validation.login.password.required}") @Size(min = ValidationConstants.MIN_PASSWORD_LENGTH, max = ValidationConstants.MAX_PASSWORD_LENGTH, message = "{"
-                + ValidationConstants.Messages.PASSWORD_SIZE + "}") String password) {
+        @NotBlank(message = "{validation.login.password.required}") @Size(min = ValidationConstants.MIN_PASSWORD_LENGTH, max = ValidationConstants.MAX_PASSWORD_LENGTH, message = "{validation.user.password.size}") String password) {
 }
