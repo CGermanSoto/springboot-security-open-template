9ec9d2f85ee8e11e50714a64aba578081e5a1c01
spacecodee <spacecodee@gmail.com>
2024-12-15 05:59:48 +0000

    Enhance validation messages in BaseUserAddVO for improved clarity and consistency

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/data/vo/user/base/BaseUserAddVO.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/data/vo/user/base/BaseUserAddVO.java
@@ -10,13 +10,19 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public abstract class BaseUserAddVO extends BaseUserUpdateVO {
-    @NotBlank(message = "{validation.user.password.required}")
-    @Size(min = ValidationConstants.MIN_PASSWORD_LENGTH, max = ValidationConstants.MAX_PASSWORD_LENGTH, message = "{"
-            + ValidationConstants.Messages.PASSWORD_SIZE + "}")
-    private String password;
+        @NotBlank(message = "{validation.user.password.required}")
+        @Size(min = ValidationConstants.MIN_PASSWORD_LENGTH, max = ValidationConstants.MAX_PASSWORD_LENGTH, message = "{validation.user.password.size,"
+                        +
+                        "${validatedValue}," +
+                        "${min}," +
+                        "${max}}")
+        private String password;
 
-    @NotBlank(message = "{validation.user.repeat.password.required}")
-    @Size(min = ValidationConstants.MIN_PASSWORD_LENGTH, max = ValidationConstants.MAX_PASSWORD_LENGTH, message = "{"
-            + ValidationConstants.Messages.REPEAT_PASSWORD_SIZE + "}")
-    private String repeatPassword;
+        @NotBlank(message = "{validation.user.repeat.password.required}")
+        @Size(min = ValidationConstants.MIN_PASSWORD_LENGTH, max = ValidationConstants.MAX_PASSWORD_LENGTH, message = "{validation.user.repeat.password.size,"
+                        +
+                        "${validatedValue}," +
+                        "${min}," +
+                        "${max}}")
+        private String repeatPassword;
 }
