fe3b67de21e7ba31f770b23bcdb2d600790557f9
spacecodee <spacecodee@gmail.com>
2024-12-15 05:59:34 +0000

    Enhance validation messages in OperationVO for improved clarity and consistency

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/data/vo/core/OperationVO.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/data/vo/core/OperationVO.java
@@ -1,24 +1,43 @@
-// OperationVO.java
 package com.spacecodee.springbootsecurityopentemplate.data.vo.core;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import com.spacecodee.springbootsecurityopentemplate.constants.ValidationConstants;
+
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class OperationVO {
-    @NotBlank(message = "{validation.operation.tag.required}")
+    @NotBlank(message = "{validation.operation.tag.required," +
+            "${validatedValue}")
+    @Size(min = ValidationConstants.MIN_NAME_LENGTH, max = ValidationConstants.MAX_NAME_LENGTH, message = "{validation.operation.tag.size,"
+            +
+            "${validatedValue}," +
+            "${min}," +
+            "${max}}")
     private String tag;
 
-    @NotBlank(message = "{validation.operation.path.required}")
+    @Pattern(regexp = "^$|^/.*$", message = "{validation.operation.path.pattern," +
+            "${validatedValue}")
     private String path;
 
-    @NotBlank(message = "{validation.operation.method.required}")
+    @NotBlank(message = "{validation.operation.method.required," +
+            "${validatedValue}")
+    @Pattern(regexp = "^(GET|POST|PUT|DELETE|PATCH)$", message = "{validation.operation.method.pattern," +
+            "${validatedValue}")
     private String httpMethod;
 
-    @NotNull(message = "{validation.operation.permit.all.required}")
+    @NotNull(message = "{validation.operation.permit.all.required," +
+            "${validatedValue}")
     private Boolean permitAll;
 
-    @NotNull(message = "{validation.operation.module.required}")
+    @NotNull(message = "{validation.operation.module.required," +
+            "${validatedValue}")
+    @Min(value = 1, message = "{validation.operation.module.min," +
+            "${validatedValue}")
     private Integer moduleId;
 }
