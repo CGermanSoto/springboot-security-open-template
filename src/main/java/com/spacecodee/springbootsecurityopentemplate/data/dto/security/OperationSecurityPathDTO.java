b49e4fe6facebe7f842f10f73dbd4a31e12479a7
spacecodee <spacecodee@gmail.com>
2025-02-12 17:17:22 +0000

    Added OperationSecurityPath because It's related to the security path that I'm going to use in the security layer later

---
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/data/dto/security/OperationSecurityPathDTO.java
@@ -0,0 +1,11 @@
+package com.spacecodee.springbootsecurityopentemplate.data.dto.security;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class OperationSecurityPathDTO {
+
+    String fullPath;
+}
