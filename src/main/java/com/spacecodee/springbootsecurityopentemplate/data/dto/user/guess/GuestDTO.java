f7b6c7fe5dda86a3c772c388b9db1f31c352ba88
spacecodee <spacecodee@gmail.com>
2025-02-12 17:27:56 +0000

    Added DTOs for the Guess, a basic DTO and a Detail DTO to show the necessary info

---
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/data/dto/user/guess/GuestDTO.java
@@ -0,0 +1,26 @@
+package com.spacecodee.springbootsecurityopentemplate.data.dto.user.guess;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class GuestDTO {
+
+    private Integer id;
+
+    private String username;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private String profilePicturePath;
+
+    private Boolean status;
+
+}
