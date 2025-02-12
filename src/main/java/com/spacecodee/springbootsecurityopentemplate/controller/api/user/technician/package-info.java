bd03d9d10fa7b1c29f8778c5981f6fdadd97e34a
spacecodee <spacecodee@gmail.com>
2025-02-12 16:07:34 +0000

    Deleted all about technician. The project's changing the roles. Now, the project has: system admin, developer, manager, editor, viewer and guess

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/controller/api/user/technician/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Contains controllers for managing technician users.
- * Includes CRUD operations and technician-specific functionality.
- */
-package com.spacecodee.springbootsecurityopentemplate.controller.api.user.technician;
