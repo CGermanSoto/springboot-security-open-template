8020ab342be4a8680ff971dff72a896d9ab46eef
spacecodee <spacecodee@gmail.com>
2025-02-12 16:11:36 +0000

    Deleted all about developer UVO and AVO, the names have changed to a better name. The project's changing the roles. Now, the project has: system admin, developer, manager, editor, viewer and guess

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/mappers/basic/IDeveloperMapper.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/mappers/basic/IDeveloperMapper.java
@@ -1,27 +1,11 @@
-// IDeveloperMapper.java
 package com.spacecodee.springbootsecurityopentemplate.mappers.basic;
 
-import com.spacecodee.springbootsecurityopentemplate.data.dto.user.DeveloperDTO;
-import com.spacecodee.springbootsecurityopentemplate.data.vo.user.developer.DeveloperAVO;
-import com.spacecodee.springbootsecurityopentemplate.data.vo.user.developer.DeveloperUVO;
-import com.spacecodee.springbootsecurityopentemplate.persistence.entity.UserEntity;
-import org.mapstruct.*;
-
-import java.util.List;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING, uses = {
         IRoleMapper.class})
 public interface IDeveloperMapper {
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "roleEntity", ignore = true)
-    UserEntity voToEntity(DeveloperAVO developerAVO);
-
-    @Mapping(target = "roleDTO", source = "roleEntity")
-    DeveloperDTO toDto(UserEntity userEntity);
-
-    List<DeveloperDTO> toDtoList(List<UserEntity> userEntities);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    UserEntity updateEntity(@MappingTarget UserEntity userEntity, DeveloperUVO developerUVO);
 }
--- src/main/java/com/spacecodee/springbootsecurityopentemplate/service/validation/impl/UserValidationServiceImpl.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/service/validation/impl/UserValidationServiceImpl.java
@@ -1,14 +1,10 @@
 package com.spacecodee.springbootsecurityopentemplate.service.validation.impl;
 
-import com.spacecodee.springbootsecurityopentemplate.data.vo.user.developer.DeveloperUVO;
-import com.spacecodee.springbootsecurityopentemplate.data.vo.user.technician.TechnicianUVO;
 import com.spacecodee.springbootsecurityopentemplate.exceptions.util.ExceptionShortComponent;
-import com.spacecodee.springbootsecurityopentemplate.persistence.entity.UserEntity;
 import com.spacecodee.springbootsecurityopentemplate.persistence.repository.IUserRepository;
 import com.spacecodee.springbootsecurityopentemplate.service.validation.IUserValidationService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -17,63 +13,6 @@ import org.springframework.stereotype.Service;
 public class UserValidationServiceImpl implements IUserValidationService {
     private final IUserRepository userRepository;
     private final ExceptionShortComponent exceptionComponent;
-
-    @Override
-    public boolean checkAndUpdateUserChanges(@NotNull Object userVO, @NotNull UserEntity existingUser) {
-        boolean hasChanges = false;
-
-        String username;
-        String fullname;
-        String lastname;
-
-        switch (userVO) {
-            case DeveloperUVO developerUVO -> {
-                username = developerUVO.getUsername();
-                fullname = developerUVO.getFullname();
-                lastname = developerUVO.getLastname();
-            }
-            case TechnicianUVO technicianUVO -> {
-                username = technicianUVO.getUsername();
-                fullname = technicianUVO.getFullname();
-                lastname = technicianUVO.getLastname();
-            }
-            default -> throw new IllegalArgumentException("Unsupported user type");
-        }
-
-        if (!existingUser.getUsername().equals(username)) {
-            existingUser.setUsername(username);
-            hasChanges = true;
-        }
-
-        if (!existingUser.getFullname().equals(fullname)) {
-            existingUser.setFullname(fullname);
-            hasChanges = true;
-        }
-
-        if (!existingUser.getLastname().equals(lastname)) {
-            existingUser.setLastname(lastname);
-            hasChanges = true;
-        }
-
-        return hasChanges;
-    }
-
-    @Override
-    public UserEntity validateUserUpdate(int id, String newUsername, String messagePrefix, String locale) {
-        if (id <= 0) {
-            throw this.exceptionComponent.invalidParameterException(messagePrefix + ".invalid.id", locale);
-        }
-
-        var existingUser = this.userRepository.findById(id)
-                .orElseThrow(() -> this.exceptionComponent.doNotExistsByIdException(messagePrefix + ".not.exists.by.id",
-                        locale));
-
-        if (!existingUser.getUsername().equals(newUsername)) {
-            validateUsername(newUsername, messagePrefix, locale);
-        }
-
-        return existingUser;
-    }
 
     @Override
     public void validateUsername(String username, String messagePrefix, String locale) {
