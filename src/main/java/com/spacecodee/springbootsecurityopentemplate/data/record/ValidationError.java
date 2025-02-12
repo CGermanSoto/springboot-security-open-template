b94c94dafb71ae9c7725a645349feb9b5d29c9f7
spacecodee <spacecodee@gmail.com>
2025-02-12 16:44:01 +0000

    Updated the ValidationError to validate and add new useful methods, also deleted the GlobalStatusEnum.java because it isn't necessary for this API at this time

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/data/record/ValidationError.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/data/record/ValidationError.java
@@ -1,9 +1,62 @@
 package com.spacecodee.springbootsecurityopentemplate.data.record;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Arrays;
+import java.util.Objects;
+
 public record ValidationError(
         String field,
         Object rejectedValue,
         String message,
-        Object[] parameters
-) {
+        Object[] parameters) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        ValidationError that = (ValidationError) o;
+        return Objects.equals(field, that.field) &&
+                Objects.equals(rejectedValue, that.rejectedValue) &&
+                Objects.equals(message, that.message) &&
+                Arrays.equals(parameters, that.parameters);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(field, rejectedValue, message);
+        result = 31 * result + Arrays.hashCode(parameters);
+        return result;
+    }
+
+    @Contract(pure = true)
+    @Override
+    public @NotNull String toString() {
+        return "ValidationError[" +
+                "field=" + field + ", " +
+                "rejectedValue=" + rejectedValue + ", " +
+                "message=" + message + ", " +
+                "parameters=" + Arrays.toString(parameters) + ']';
+    }
+
+    @JsonIgnore
+    public Object getRawRejectedValue() {
+        return rejectedValue;
+    }
+
+    @JsonValue
+    public @Nullable String getRejectedValue() {
+        if (rejectedValue instanceof MultipartFile multipartFile) {
+            return multipartFile.getOriginalFilename();
+        }
+        return rejectedValue != null ? rejectedValue.toString() : null;
+    }
 }
