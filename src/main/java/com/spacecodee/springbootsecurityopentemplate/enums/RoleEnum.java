652f8d2e8b38d8408099a7a61fcea297652435d8
spacecodee <spacecodee@gmail.com>
2025-02-13 13:54:41 +0000

    Updated RoleEnum and RolePermissionEnum to use the new roles and permissions for the API

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/enums/RoleEnum.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/enums/RoleEnum.java
@@ -13,60 +13,248 @@ import java.util.List;
 public enum RoleEnum {
 
     DEVELOPER(Arrays.asList(
-            RolePermissionEnum.REGISTER_ONE_ADMIN,
-            RolePermissionEnum.UPDATE_ONE_ADMIN,
-            RolePermissionEnum.DELETE_ONE_ADMIN,
-            RolePermissionEnum.FIND_ONE_ADMIN,
-            RolePermissionEnum.FIND_ALL_ADMINS,
+            RolePermissionEnum.CREATE_ONE_SYSTEM_ADMIN,
+            RolePermissionEnum.UPDATE_ONE_SYSTEM_ADMIN,
+            RolePermissionEnum.UPDATE_STATUS_ONE_SYSTEM_ADMIN,
+            RolePermissionEnum.DELETE_ONE_SYSTEM_ADMIN,
+            RolePermissionEnum.FIND_ONE_SYSTEM_ADMIN,
+            RolePermissionEnum.FIND_ONE_DETAIL_SYSTEM_ADMIN,
+            RolePermissionEnum.FIND_ALL_PAGE_SYSTEM_ADMINS,
 
-            RolePermissionEnum.REGISTER_ONE_DEVELOPER,
+            RolePermissionEnum.CREATE_ONE_DEVELOPER,
             RolePermissionEnum.DELETE_ONE_DEVELOPER,
             RolePermissionEnum.UPDATE_ONE_DEVELOPER,
+            RolePermissionEnum.UPDATE_STATUS_ONE_DEVELOPER,
             RolePermissionEnum.FIND_ONE_DEVELOPER,
-            RolePermissionEnum.FIND_ALL_DEVELOPERS,
+            RolePermissionEnum.FIND_ONE_DETAIL_DEVELOPER,
+            RolePermissionEnum.FIND_ALL_PAGE_DEVELOPERS,
 
-            RolePermissionEnum.SHOW_PROFILE,
+            RolePermissionEnum.CREATE_ONE_MANAGER,
+            RolePermissionEnum.DELETE_ONE_MANAGER,
+            RolePermissionEnum.UPDATE_ONE_MANAGER,
+            RolePermissionEnum.UPDATE_STATUS_ONE_MANAGER,
+            RolePermissionEnum.FIND_ONE_MANAGER,
+            RolePermissionEnum.FIND_ONE_DETAIL_MANAGER,
+            RolePermissionEnum.FIND_ALL_PAGE_MANAGERS,
+
+            RolePermissionEnum.CREATE_ONE_EDITOR,
+            RolePermissionEnum.DELETE_ONE_EDITOR,
+            RolePermissionEnum.UPDATE_ONE_EDITOR,
+            RolePermissionEnum.UPDATE_STATUS_ONE_EDITOR,
+            RolePermissionEnum.FIND_ONE_EDITOR,
+            RolePermissionEnum.FIND_ONE_DETAIL_EDITOR,
+            RolePermissionEnum.FIND_ALL_PAGE_EDITOR,
+
+            RolePermissionEnum.CREATE_ONE_VIEWER,
+            RolePermissionEnum.DELETE_ONE_VIEWER,
+            RolePermissionEnum.UPDATE_ONE_VIEWER,
+            RolePermissionEnum.UPDATE_STATUS_ONE_VIEWER,
+            RolePermissionEnum.FIND_ONE_VIEWER,
+            RolePermissionEnum.FIND_ONE_DETAIL_VIEWER,
+            RolePermissionEnum.FIND_ALL_PAGE_VIEWER,
+
+            RolePermissionEnum.CREATE_ONE_GUEST,
+            RolePermissionEnum.DELETE_ONE_GUEST,
+            RolePermissionEnum.UPDATE_ONE_GUEST,
+            RolePermissionEnum.UPDATE_STATUS_ONE_GUEST,
+            RolePermissionEnum.FIND_ONE_GUEST,
+            RolePermissionEnum.FIND_ONE_DETAIL_GUEST,
+            RolePermissionEnum.FIND_ALL_PAGE_GUEST,
+
+            RolePermissionEnum.SHOW_USER_PROFILE,
+
             RolePermissionEnum.VALIDATE_TOKEN,
             RolePermissionEnum.LOGOUT,
             RolePermissionEnum.REFRESH_TOKEN,
 
-            RolePermissionEnum.MODULE,
-            RolePermissionEnum.OPERATION,
-            RolePermissionEnum.PERMISSION,
-            RolePermissionEnum.PERMISSION_DELETE,
-            RolePermissionEnum.OPERATION_DELETE,
-            RolePermissionEnum.MODULE_DELETE
+            RolePermissionEnum.FIND_PERMISSIONS_BY_CURRENT_USER,
+
+            RolePermissionEnum.CREATE_ONE_MODULE,
+            RolePermissionEnum.UPDATE_ONE_MODULE,
+            RolePermissionEnum.DELETE_ONE_MODULE,
+            RolePermissionEnum.FIND_ONE_MODULE,
+            RolePermissionEnum.FIND_ALL_MODULE,
+            RolePermissionEnum.FIND_ALL_PAGE_MODULE,
+
+            RolePermissionEnum.CREATE_ONE_OPERATION,
+            RolePermissionEnum.UPDATE_ONE_OPERATION,
+            RolePermissionEnum.DELETE_ONE_OPERATION,
+            RolePermissionEnum.FIND_ONE_OPERATION,
+            RolePermissionEnum.FIND_ONE_DETAIL_OPERATION,
+            RolePermissionEnum.FIND_ALL_PAGE_OPERATION,
+            RolePermissionEnum.FIND_ALL_OPERATION_BY_MODULE,
+            RolePermissionEnum.FIND_ALL_OPERATION,
+
+            RolePermissionEnum.CREATE_ONE_ROLE,
+            RolePermissionEnum.UPDATE_ONE_ROLE,
+            RolePermissionEnum.DELETE_ONE_ROLE,
+            RolePermissionEnum.FIND_ONE_ROLE,
+            RolePermissionEnum.FIND_ONE_DETAIL_ROLE,
+            RolePermissionEnum.FIND_ALL_PAGE_ROLE,
+            RolePermissionEnum.FIND_ALL_ROLE,
+
+            RolePermissionEnum.CREATE_ONE_PERMISSION,
+            RolePermissionEnum.UPDATE_ONE_PERMISSION,
+            RolePermissionEnum.DELETE_ONE_PERMISSION,
+            RolePermissionEnum.FIND_ONE_PERMISSION,
+            RolePermissionEnum.FIND_ONE_DETAIL_PERMISSION,
+            RolePermissionEnum.FIND_ALL_PAGE_PERMISSION,
+            RolePermissionEnum.FIND_ALL_PERMISSION,
+            RolePermissionEnum.FIND_ALL_PERMISSION_BY_ROLE
     )),
 
-    OTI_ADMIN(Arrays.asList(
-            RolePermissionEnum.REGISTER_ONE_ADMIN,
-            RolePermissionEnum.UPDATE_ONE_ADMIN,
-            RolePermissionEnum.DELETE_ONE_ADMIN,
-            RolePermissionEnum.FIND_ONE_ADMIN,
-            RolePermissionEnum.FIND_ALL_ADMINS,
+    SYSTEM_ADMIN(Arrays.asList(
+            RolePermissionEnum.CREATE_ONE_SYSTEM_ADMIN,
+            RolePermissionEnum.UPDATE_ONE_SYSTEM_ADMIN,
+            RolePermissionEnum.UPDATE_STATUS_ONE_SYSTEM_ADMIN,
+            RolePermissionEnum.DELETE_ONE_SYSTEM_ADMIN,
+            RolePermissionEnum.FIND_ONE_SYSTEM_ADMIN,
+            RolePermissionEnum.FIND_ONE_DETAIL_SYSTEM_ADMIN,
+            RolePermissionEnum.FIND_ALL_PAGE_SYSTEM_ADMINS,
 
-            RolePermissionEnum.REGISTER_ONE_DEVELOPER,
+            RolePermissionEnum.CREATE_ONE_MANAGER,
+            RolePermissionEnum.DELETE_ONE_MANAGER,
+            RolePermissionEnum.UPDATE_ONE_MANAGER,
+            RolePermissionEnum.UPDATE_STATUS_ONE_MANAGER,
+            RolePermissionEnum.FIND_ONE_MANAGER,
+            RolePermissionEnum.FIND_ONE_DETAIL_MANAGER,
+            RolePermissionEnum.FIND_ALL_PAGE_MANAGERS,
 
-            RolePermissionEnum.SHOW_PROFILE,
+            RolePermissionEnum.CREATE_ONE_EDITOR,
+            RolePermissionEnum.DELETE_ONE_EDITOR,
+            RolePermissionEnum.UPDATE_ONE_EDITOR,
+            RolePermissionEnum.UPDATE_STATUS_ONE_EDITOR,
+            RolePermissionEnum.FIND_ONE_EDITOR,
+            RolePermissionEnum.FIND_ONE_DETAIL_EDITOR,
+            RolePermissionEnum.FIND_ALL_PAGE_EDITOR,
+
+            RolePermissionEnum.CREATE_ONE_VIEWER,
+            RolePermissionEnum.DELETE_ONE_VIEWER,
+            RolePermissionEnum.UPDATE_ONE_VIEWER,
+            RolePermissionEnum.UPDATE_STATUS_ONE_VIEWER,
+            RolePermissionEnum.FIND_ONE_VIEWER,
+            RolePermissionEnum.FIND_ONE_DETAIL_VIEWER,
+            RolePermissionEnum.FIND_ALL_PAGE_VIEWER,
+
+            RolePermissionEnum.CREATE_ONE_GUEST,
+            RolePermissionEnum.DELETE_ONE_GUEST,
+            RolePermissionEnum.UPDATE_ONE_GUEST,
+            RolePermissionEnum.UPDATE_STATUS_ONE_GUEST,
+            RolePermissionEnum.FIND_ONE_GUEST,
+            RolePermissionEnum.FIND_ONE_DETAIL_GUEST,
+            RolePermissionEnum.FIND_ALL_PAGE_GUEST,
+
+            RolePermissionEnum.SHOW_USER_PROFILE,
+
             RolePermissionEnum.VALIDATE_TOKEN,
             RolePermissionEnum.LOGOUT,
-            RolePermissionEnum.REFRESH_TOKEN
+            RolePermissionEnum.REFRESH_TOKEN,
+
+            RolePermissionEnum.FIND_PERMISSIONS_BY_CURRENT_USER
     )),
 
-    TECHNICIAN(Arrays.asList(
-            RolePermissionEnum.REGISTER_ONE_ADMIN,
+    MANAGER(Arrays.asList(
+            RolePermissionEnum.FIND_ONE_MANAGER,
+            RolePermissionEnum.FIND_ONE_DETAIL_MANAGER,
+            RolePermissionEnum.FIND_ALL_PAGE_MANAGERS,
 
-            RolePermissionEnum.SHOW_PROFILE,
+            RolePermissionEnum.CREATE_ONE_EDITOR,
+            RolePermissionEnum.DELETE_ONE_EDITOR,
+            RolePermissionEnum.UPDATE_ONE_EDITOR,
+            RolePermissionEnum.UPDATE_STATUS_ONE_EDITOR,
+            RolePermissionEnum.FIND_ONE_EDITOR,
+            RolePermissionEnum.FIND_ONE_DETAIL_EDITOR,
+            RolePermissionEnum.FIND_ALL_PAGE_EDITOR,
+
+            RolePermissionEnum.CREATE_ONE_VIEWER,
+            RolePermissionEnum.DELETE_ONE_VIEWER,
+            RolePermissionEnum.UPDATE_ONE_VIEWER,
+            RolePermissionEnum.UPDATE_STATUS_ONE_VIEWER,
+            RolePermissionEnum.FIND_ONE_VIEWER,
+            RolePermissionEnum.FIND_ONE_DETAIL_VIEWER,
+            RolePermissionEnum.FIND_ALL_PAGE_VIEWER,
+
+            RolePermissionEnum.CREATE_ONE_GUEST,
+            RolePermissionEnum.DELETE_ONE_GUEST,
+            RolePermissionEnum.UPDATE_ONE_GUEST,
+            RolePermissionEnum.UPDATE_STATUS_ONE_GUEST,
+            RolePermissionEnum.FIND_ONE_GUEST,
+            RolePermissionEnum.FIND_ONE_DETAIL_GUEST,
+            RolePermissionEnum.FIND_ALL_PAGE_GUEST,
+
+            RolePermissionEnum.SHOW_USER_PROFILE,
+
             RolePermissionEnum.VALIDATE_TOKEN,
             RolePermissionEnum.LOGOUT,
-            RolePermissionEnum.REFRESH_TOKEN
+            RolePermissionEnum.REFRESH_TOKEN,
+
+            RolePermissionEnum.FIND_PERMISSIONS_BY_CURRENT_USER
     )),
 
-    CUSTOMER(List.of(
-            RolePermissionEnum.SHOW_PROFILE,
+    EDITOR(List.of(
+            RolePermissionEnum.FIND_ONE_EDITOR,
+            RolePermissionEnum.FIND_ONE_DETAIL_EDITOR,
+            RolePermissionEnum.FIND_ALL_PAGE_EDITOR,
+
+            RolePermissionEnum.CREATE_ONE_VIEWER,
+            RolePermissionEnum.DELETE_ONE_VIEWER,
+            RolePermissionEnum.UPDATE_ONE_VIEWER,
+            RolePermissionEnum.UPDATE_STATUS_ONE_VIEWER,
+            RolePermissionEnum.FIND_ONE_VIEWER,
+            RolePermissionEnum.FIND_ONE_DETAIL_VIEWER,
+            RolePermissionEnum.FIND_ALL_PAGE_VIEWER,
+
+            RolePermissionEnum.CREATE_ONE_GUEST,
+            RolePermissionEnum.DELETE_ONE_GUEST,
+            RolePermissionEnum.UPDATE_ONE_GUEST,
+            RolePermissionEnum.UPDATE_STATUS_ONE_GUEST,
+            RolePermissionEnum.FIND_ONE_GUEST,
+            RolePermissionEnum.FIND_ONE_DETAIL_GUEST,
+            RolePermissionEnum.FIND_ALL_PAGE_GUEST,
+
+            RolePermissionEnum.SHOW_USER_PROFILE,
+
             RolePermissionEnum.VALIDATE_TOKEN,
             RolePermissionEnum.LOGOUT,
-            RolePermissionEnum.REFRESH_TOKEN
+            RolePermissionEnum.REFRESH_TOKEN,
+
+            RolePermissionEnum.FIND_PERMISSIONS_BY_CURRENT_USER
+    )),
+
+    VIEWER(List.of(
+            RolePermissionEnum.FIND_ONE_VIEWER,
+            RolePermissionEnum.FIND_ONE_DETAIL_VIEWER,
+            RolePermissionEnum.FIND_ALL_PAGE_VIEWER,
+
+            RolePermissionEnum.FIND_ONE_GUEST,
+            RolePermissionEnum.FIND_ONE_DETAIL_GUEST,
+            RolePermissionEnum.FIND_ALL_PAGE_GUEST,
+
+            RolePermissionEnum.SHOW_USER_PROFILE,
+
+            RolePermissionEnum.VALIDATE_TOKEN,
+            RolePermissionEnum.LOGOUT,
+            RolePermissionEnum.REFRESH_TOKEN,
+
+            RolePermissionEnum.FIND_PERMISSIONS_BY_CURRENT_USER
+    )),
+
+    GUEST(List.of(
+            RolePermissionEnum.FIND_ONE_VIEWER,
+            RolePermissionEnum.FIND_ONE_DETAIL_VIEWER,
+            RolePermissionEnum.FIND_ALL_PAGE_VIEWER,
+
+            RolePermissionEnum.FIND_ONE_GUEST,
+            RolePermissionEnum.FIND_ONE_DETAIL_GUEST,
+            RolePermissionEnum.FIND_ALL_PAGE_GUEST,
+
+            RolePermissionEnum.SHOW_USER_PROFILE,
+
+            RolePermissionEnum.VALIDATE_TOKEN,
+            RolePermissionEnum.LOGOUT,
+            RolePermissionEnum.REFRESH_TOKEN,
+
+            RolePermissionEnum.FIND_PERMISSIONS_BY_CURRENT_USER
     ));
 
     private List<RolePermissionEnum> permissions;
