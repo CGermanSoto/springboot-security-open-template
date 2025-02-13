39d965bc1ba9db99ca857edc423e4b92f27eb39f
spacecodee <spacecodee@gmail.com>
2025-02-13 22:48:45 +0000

    Updated exception

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/exceptions/auth/jwt/JwtTokenHeaderAlgException.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/exceptions/auth/jwt/JwtTokenHeaderAlgException.java
@@ -1,10 +1,4 @@
 package com.spacecodee.springbootsecurityopentemplate.exceptions.auth.jwt;
 
-import com.spacecodee.springbootsecurityopentemplate.exceptions.base.BaseException;
-
-public class JwtTokenHeaderAlgException extends BaseException {
-
-    public JwtTokenHeaderAlgException(String messageKey, String locale, Object... args) {
-        super(messageKey, locale, args);
-    }
+public class JwtTokenHeaderALgException {
 }
