cc70c6aa8f78ce544b4285a2f033e4d3a085b5b3
spacecodee <spacecodee@gmail.com>
2025-02-13 21:54:22 +0000

    Deleted unused controllers

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/controller/api/enpoint/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Contains controllers for managing endpoints and permissions.
- * Handles operations for modules, operations, and role-based access control.
- */
-package com.spacecodee.springbootsecurityopentemplate.controller.api.enpoint;
