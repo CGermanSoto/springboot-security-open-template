44460f40bde2b2b7e0cbe5ca7c7f137bb2d5888d
spacecodee <spacecodee@gmail.com>
2025-02-13 17:43:38 +0000

    Delete all unused services because the API has changed with new requirements

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/service/validation/IUserValidationService.java
+++
@@ -1,6 +0,0 @@
-package com.spacecodee.springbootsecurityopentemplate.service.validation;
-
-public interface IUserValidationService {
-
-    void validateUsername(String username, String messagePrefix, String locale);
-}
