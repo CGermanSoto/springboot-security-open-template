5c6a7e66a81f6987f7a5485122598ddcac63848b
spacecodee <spacecodee@gmail.com>
2025-03-04 15:29:37 +0000

    Remove unused publishTokenEvent method from TokenEventBusServiceImpl

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/service/security/token/event/impl/TokenEventBusServiceImpl.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/service/security/token/event/impl/TokenEventBusServiceImpl.java
@@ -1,6 +1,5 @@
 package com.spacecodee.springbootsecurityopentemplate.service.security.token.event.impl;
 
-import com.spacecodee.springbootsecurityopentemplate.data.record.TokenEvent;
 import com.spacecodee.springbootsecurityopentemplate.data.record.TokenStateChangeEvent;
 import com.spacecodee.springbootsecurityopentemplate.enums.TokenStateEnum;
 import com.spacecodee.springbootsecurityopentemplate.service.security.token.event.ITokenEventBusService;
@@ -13,11 +12,6 @@ import org.springframework.stereotype.Service;
 public class TokenEventBusServiceImpl implements ITokenEventBusService {
 
     private final ApplicationEventPublisher eventPublisher;
-
-    @Override
-    public void publishTokenEvent(String token, String operation, String reason) {
-        eventPublisher.publishEvent(new TokenEvent(token, operation, reason));
-    }
 
     @Override
     public void publishTokenStateChange(String token, TokenStateEnum state, String reason) {
