e609e8d81d38671bd8826afe0cb94c8ddec135ec
spacecodee <spacecodee@gmail.com>
2025-03-04 15:29:34 +0000

    Remove unused publishTokenEvent method from ITokenEventBusService interface

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/service/security/token/event/ITokenEventBusService.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/service/security/token/event/ITokenEventBusService.java
@@ -4,8 +4,6 @@ import com.spacecodee.springbootsecurityopentemplate.enums.TokenStateEnum;
 
 public interface ITokenEventBusService {
 
-    void publishTokenEvent(String token, String operation, String reason);
-
     void publishTokenStateChange(String token, TokenStateEnum state, String reason);
 
 }
