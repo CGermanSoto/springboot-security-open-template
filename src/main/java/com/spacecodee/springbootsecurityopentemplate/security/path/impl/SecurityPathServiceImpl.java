c655b0b03d707f7489302800500473e496c08737
spacecodee <spacecodee@gmail.com>
2025-03-11 14:42:16 +0000

    Refactor SecurityPathServiceImpl to utilize caching for public path retrieval

--- src/main/java/com/spacecodee/springbootsecurityopentemplate/security/path/impl/SecurityPathServiceImpl.java
+++ src/main/java/com/spacecodee/springbootsecurityopentemplate/security/path/impl/SecurityPathServiceImpl.java
@@ -1,11 +1,9 @@
 package com.spacecodee.springbootsecurityopentemplate.security.path.impl;
 
+import com.spacecodee.springbootsecurityopentemplate.cache.ISecurityCacheService;
 import com.spacecodee.springbootsecurityopentemplate.constants.SecurityConstants;
-import com.spacecodee.springbootsecurityopentemplate.data.dto.security.OperationSecurityPathDTO;
 import com.spacecodee.springbootsecurityopentemplate.security.path.ISecurityPathService;
-import com.spacecodee.springbootsecurityopentemplate.service.security.operation.IOperationSecurityService;
 import com.spacecodee.springbootsecurityopentemplate.utils.PathUtils;
-import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,7 +11,6 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 import org.springframework.util.AntPathMatcher;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -21,27 +18,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SecurityPathServiceImpl implements ISecurityPathService {
 
-    private final IOperationSecurityService operationSecurityService;
+    private final ISecurityCacheService securityCacheService;
     private final AntPathMatcher pathMatcher = new AntPathMatcher();
-    private List<String> publicPaths = new ArrayList<>();
-
-    @PostConstruct
-    public void init() {
-        this.updatePublicPaths();
-    }
-
-    private void updatePublicPaths() {
-        this.publicPaths = this.operationSecurityService.findByPublicAccess()
-                .stream()
-                .map(OperationSecurityPathDTO::getFullPath)
-                .toList();
-        log.info("Public paths updated: {}", this.publicPaths);
-    }
 
     @Override
     public boolean isPublicPath(@NotNull String path) {
         String normalizedPath = PathUtils.normalizeRequestPath(path);
-        return this.publicPaths.stream()
+        List<String> publicPaths = this.securityCacheService.getPublicPaths();
+        return publicPaths.stream()
                 .anyMatch(publicPath -> this.pathMatcher.match(publicPath, normalizedPath));
     }
 
@@ -71,5 +55,5 @@ public class SecurityPathServiceImpl implements ISecurityPathService {
     public boolean isRefreshTokenPath(@NotNull String path) {
         return path.endsWith(SecurityConstants.REFRESH_TOKEN_PATH);
     }
-    
+
 }
